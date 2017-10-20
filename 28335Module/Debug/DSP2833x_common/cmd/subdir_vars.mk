2e73b165745eacf5351d0de873f63f8b78b7edd0
tengyong1012 <tengyong1012@163.com>
2017-10-20 17:50:55 +0800

    add scic
    oops,can't enter interrupt

---
+++ 28335Module/Debug/DSP2833x_common/cmd/subdir_vars.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../DSP2833x_common/cmd/28335_RAM_lnk.cmd \
+../DSP2833x_common/cmd/F28335.cmd 
+
+
---
+++ 28335Module/Debug/DSP2833x_common/source/subdir_vars.mk
@@ -0,0 +1,128 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+ASM_SRCS += \
+../DSP2833x_common/source/DSP2833x_ADC_cal.asm \
+../DSP2833x_common/source/DSP2833x_CodeStartBranch.asm \
+../DSP2833x_common/source/DSP2833x_usDelay.asm 
+
+C_SRCS += \
+../DSP2833x_common/source/DSP2833x_Adc.c \
+../DSP2833x_common/source/DSP2833x_CpuTimers.c \
+../DSP2833x_common/source/DSP2833x_DefaultIsr.c \
+../DSP2833x_common/source/DSP2833x_ECan.c \
+../DSP2833x_common/source/DSP2833x_ECap.c \
+../DSP2833x_common/source/DSP2833x_EPwm.c \
+../DSP2833x_common/source/DSP2833x_EQep.c \
+../DSP2833x_common/source/DSP2833x_Gpio.c \
+../DSP2833x_common/source/DSP2833x_I2C.c \
+../DSP2833x_common/source/DSP2833x_PieCtrl.c \
+../DSP2833x_common/source/DSP2833x_PieVect.c \
+../DSP2833x_common/source/DSP2833x_Sci.c \
+../DSP2833x_common/source/DSP2833x_Spi.c \
+../DSP2833x_common/source/DSP2833x_SysCtrl.c 
+
+OBJS += \
+./DSP2833x_common/source/DSP2833x_ADC_cal.obj \
+./DSP2833x_common/source/DSP2833x_Adc.obj \
+./DSP2833x_common/source/DSP2833x_CodeStartBranch.obj \
+./DSP2833x_common/source/DSP2833x_CpuTimers.obj \
+./DSP2833x_common/source/DSP2833x_DefaultIsr.obj \
+./DSP2833x_common/source/DSP2833x_ECan.obj \
+./DSP2833x_common/source/DSP2833x_ECap.obj \
+./DSP2833x_common/source/DSP2833x_EPwm.obj \
+./DSP2833x_common/source/DSP2833x_EQep.obj \
+./DSP2833x_common/source/DSP2833x_Gpio.obj \
+./DSP2833x_common/source/DSP2833x_I2C.obj \
+./DSP2833x_common/source/DSP2833x_PieCtrl.obj \
+./DSP2833x_common/source/DSP2833x_PieVect.obj \
+./DSP2833x_common/source/DSP2833x_Sci.obj \
+./DSP2833x_common/source/DSP2833x_Spi.obj \
+./DSP2833x_common/source/DSP2833x_SysCtrl.obj \
+./DSP2833x_common/source/DSP2833x_usDelay.obj 
+
+ASM_DEPS += \
+./DSP2833x_common/source/DSP2833x_ADC_cal.pp \
+./DSP2833x_common/source/DSP2833x_CodeStartBranch.pp \
+./DSP2833x_common/source/DSP2833x_usDelay.pp 
+
+C_DEPS += \
+./DSP2833x_common/source/DSP2833x_Adc.pp \
+./DSP2833x_common/source/DSP2833x_CpuTimers.pp \
+./DSP2833x_common/source/DSP2833x_DefaultIsr.pp \
+./DSP2833x_common/source/DSP2833x_ECan.pp \
+./DSP2833x_common/source/DSP2833x_ECap.pp \
+./DSP2833x_common/source/DSP2833x_EPwm.pp \
+./DSP2833x_common/source/DSP2833x_EQep.pp \
+./DSP2833x_common/source/DSP2833x_Gpio.pp \
+./DSP2833x_common/source/DSP2833x_I2C.pp \
+./DSP2833x_common/source/DSP2833x_PieCtrl.pp \
+./DSP2833x_common/source/DSP2833x_PieVect.pp \
+./DSP2833x_common/source/DSP2833x_Sci.pp \
+./DSP2833x_common/source/DSP2833x_Spi.pp \
+./DSP2833x_common/source/DSP2833x_SysCtrl.pp 
+
+C_DEPS__QUOTED += \
+"DSP2833x_common\source\DSP2833x_Adc.pp" \
+"DSP2833x_common\source\DSP2833x_CpuTimers.pp" \
+"DSP2833x_common\source\DSP2833x_DefaultIsr.pp" \
+"DSP2833x_common\source\DSP2833x_ECan.pp" \
+"DSP2833x_common\source\DSP2833x_ECap.pp" \
+"DSP2833x_common\source\DSP2833x_EPwm.pp" \
+"DSP2833x_common\source\DSP2833x_EQep.pp" \
+"DSP2833x_common\source\DSP2833x_Gpio.pp" \
+"DSP2833x_common\source\DSP2833x_I2C.pp" \
+"DSP2833x_common\source\DSP2833x_PieCtrl.pp" \
+"DSP2833x_common\source\DSP2833x_PieVect.pp" \
+"DSP2833x_common\source\DSP2833x_Sci.pp" \
+"DSP2833x_common\source\DSP2833x_Spi.pp" \
+"DSP2833x_common\source\DSP2833x_SysCtrl.pp" 
+
+OBJS__QUOTED += \
+"DSP2833x_common\source\DSP2833x_ADC_cal.obj" \
+"DSP2833x_common\source\DSP2833x_Adc.obj" \
+"DSP2833x_common\source\DSP2833x_CodeStartBranch.obj" \
+"DSP2833x_common\source\DSP2833x_CpuTimers.obj" \
+"DSP2833x_common\source\DSP2833x_DefaultIsr.obj" \
+"DSP2833x_common\source\DSP2833x_ECan.obj" \
+"DSP2833x_common\source\DSP2833x_ECap.obj" \
+"DSP2833x_common\source\DSP2833x_EPwm.obj" \
+"DSP2833x_common\source\DSP2833x_EQep.obj" \
+"DSP2833x_common\source\DSP2833x_Gpio.obj" \
+"DSP2833x_common\source\DSP2833x_I2C.obj" \
+"DSP2833x_common\source\DSP2833x_PieCtrl.obj" \
+"DSP2833x_common\source\DSP2833x_PieVect.obj" \
+"DSP2833x_common\source\DSP2833x_Sci.obj" \
+"DSP2833x_common\source\DSP2833x_Spi.obj" \
+"DSP2833x_common\source\DSP2833x_SysCtrl.obj" \
+"DSP2833x_common\source\DSP2833x_usDelay.obj" 
+
+ASM_DEPS__QUOTED += \
+"DSP2833x_common\source\DSP2833x_ADC_cal.pp" \
+"DSP2833x_common\source\DSP2833x_CodeStartBranch.pp" \
+"DSP2833x_common\source\DSP2833x_usDelay.pp" 
+
+ASM_SRCS__QUOTED += \
+"../DSP2833x_common/source/DSP2833x_ADC_cal.asm" \
+"../DSP2833x_common/source/DSP2833x_CodeStartBranch.asm" \
+"../DSP2833x_common/source/DSP2833x_usDelay.asm" 
+
+C_SRCS__QUOTED += \
+"../DSP2833x_common/source/DSP2833x_Adc.c" \
+"../DSP2833x_common/source/DSP2833x_CpuTimers.c" \
+"../DSP2833x_common/source/DSP2833x_DefaultIsr.c" \
+"../DSP2833x_common/source/DSP2833x_ECan.c" \
+"../DSP2833x_common/source/DSP2833x_ECap.c" \
+"../DSP2833x_common/source/DSP2833x_EPwm.c" \
+"../DSP2833x_common/source/DSP2833x_EQep.c" \
+"../DSP2833x_common/source/DSP2833x_Gpio.c" \
+"../DSP2833x_common/source/DSP2833x_I2C.c" \
+"../DSP2833x_common/source/DSP2833x_PieCtrl.c" \
+"../DSP2833x_common/source/DSP2833x_PieVect.c" \
+"../DSP2833x_common/source/DSP2833x_Sci.c" \
+"../DSP2833x_common/source/DSP2833x_Spi.c" \
+"../DSP2833x_common/source/DSP2833x_SysCtrl.c" 
+
+
